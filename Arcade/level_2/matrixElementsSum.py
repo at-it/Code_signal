1e796a63ca05b2b3e577de94e3e9d3621b60c50c
Adrian <adrian.terebinski@gmail.com>
2022-08-29 21:18:53 +0100

    Minesweeper solution implemented

--- Arcade/level_2/matrixElementsSum.py
+++ Arcade/level_2/matrixElementsSum.py
@@ -6,22 +6,23 @@ Given matrix, a rectangular matrix of integers, where each value represents the 
 your task is to return the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that don't appear below a 0)."""
 
 
-matrix = [[0, 1, 1, 2], 
-          [0, 5, 0, 0], 
+matrix = [[0, 1, 1, 2],
+          [0, 5, 0, 0],
           [2, 0, 3, 3]]
 
 
 def solution(matrix):
     no_of_rows = len(matrix)
     no_of_cols = len(matrix[0])
-    
+
     result = 0
-    
-    for i in range(no_of_cols):  
+
+    for i in range(no_of_cols):
         for j in range(no_of_rows):
             if matrix[j][i] == 0:
                 break
-            result+=matrix[j][i]
+            result += matrix[j][i]
     return result
+
 
 print(solution(matrix))
--- Arcade/level_5/minesweeper.py
+++ Arcade/level_5/minesweeper.py
@@ -8,54 +8,44 @@ matrix = [[True, False, False],
           [False, True, False],
           [False, False, False]]
 
-
 matrix_2 = [[False, False, False],
             [False, False, False]]
 
-matrix_3 = [['true', 'false', 'false', 'true'],
-            ['false', 'false', 'true', 'false'],
-            ['true', 'true', 'false', 'true']]
+matrix_3 = [[True, False, False, True],
+            [False, False, True, False],
+            [True, True, False, True]]
 
 
 def solution(matrix):
-
-    clean_up_matrix(matrix)
 
     final_matrix = copy.deepcopy(matrix)
     matrix_height = len(final_matrix)
     matrix_width = len(final_matrix[0])
 
-    for col in range(matrix_width):
-        for row in range(matrix_height):
-            final_matrix[row][col] = calculate_surrounding_mines(
-                matrix, row, col)
+    for row in range(matrix_height):
+        for col in range(matrix_width):
+            final_matrix[row][col] = calculate_surrounding_mines(matrix, row, col)
 
     return final_matrix
 
 
 def calculate_surrounding_mines(matrix, row, col):
-    temp_matrix = copy.deepcopy(matrix)
 
-    # making sure that we neglect current position
-    temp_matrix[row][col] = False
-
+    # setup of moving window
     min_row = max(row - 1, 0)
-    max_row = min(min_row + 1, len(temp_matrix)) + 1
+    max_row = row + 2
     min_col = max(col - 1, 0)
-    max_col = min(min_col + 1, len(temp_matrix[0])) + 1
+    max_col = col + 2
 
-    window = [x[min_col:max_col] for x in temp_matrix[min_row:max_row]]
+    window = [x[min_col:max_col] for x in matrix[min_row:max_row]]
+    
+    # making sure that we neglect current position
+    window[row - min_row][col - min_col] = False
+    
+    # flattenning for summing 
     window_flat = [s for t in window for s in t]
 
     return sum(window_flat)
 
-
-def clean_up_matrix(matrix):
-    matrix_height = len(matrix)
-    matrix_width = len(matrix[0])
-    new_matrix = [True if s == 'true' else False if s ==
-        'false' else 'error' for t in matrix for s in t]
-    
-    pass
 
 print(solution(matrix_3))
