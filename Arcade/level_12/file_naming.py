0865259d08cc459a3bf352e107e2dc9efb5b6cbe
Adrian <adrian.terebinski@gmail.com>
2022-09-01 20:06:01 +0100

    Solution file_naming implemented

--- Arcade/level_12/file_naming.py
+++ Arcade/level_12/file_naming.py
@@ -5,3 +5,21 @@ such that the obtained name is not used yet.
 Return an array of names that will be given to the files.'''
 
 
+def solution(names):
+
+    result = []
+
+    for name in names:
+        if name not in result:
+            result.append(name)
+        else:
+            counter = 1
+            new_name = name + '(' + str(counter) + ')'
+            
+            while new_name in result:
+                counter += 1
+                new_name = name + '(' + str(counter) + ')'
+
+            result.append(new_name)
+            
+    return result
