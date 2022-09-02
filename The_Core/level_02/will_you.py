cec4fc930a570306d00abd59c9162f036aa49f7a
Adrian <adrian.terebinski@gmail.com>
2022-09-02 12:03:06 +0100

    Solution will_you implemented

---
+++ The_Core/level_02/will_you.py
@@ -0,0 +1,17 @@
+'''Once Mary heard a famous song, and a line from it stuck in her head. That line was "Will you still love me when I'm no longer young and beautiful?".
+Mary believes that a person is loved if and only if he/she is both young and beautiful, but this is quite a depressing thought, 
+so she wants to put her belief to the test.
+
+Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+
+A person contradicts Mary's belief if one of the following statements is true:
+
+they are young and beautiful but not loved;
+they are loved but not young or not beautiful.'''
+
+
+def solution(young, beautiful, loved):
+    condition_1 = young and beautiful and not loved
+    condition_2 = loved and (not young or not beautiful)
+
+    return condition_1 or condition_2
